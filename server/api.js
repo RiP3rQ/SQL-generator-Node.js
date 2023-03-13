726127372e975d9e4a2045b0239e0ad97310b772
RiP3rQ <rafalpompa2000@gmail.com>
2023-03-13 21:03:01 +0100

    connect to oraclesql using node.js

---
+++ server/api.js
@@ -0,0 +1,22 @@
+const oracleDb = require("oracledb");
+oracleDb.outFormat = oracleDb.OUT_FORMAT_OBJECT;
+
+async function connect() {
+  let con;
+
+  try {
+    con = await oracleDb.getConnection({
+      user: "s101187",
+      password: "s101187",
+      connectString: "217.173.198.135:1521/tpdb",
+    });
+
+    const data = await con.execute("SELECT * FROM klienci");
+    // const ostatniRekord = data.rows.slice(-1)[0].ID_KLIENTA;
+    console.log(data.rows);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+connect();
